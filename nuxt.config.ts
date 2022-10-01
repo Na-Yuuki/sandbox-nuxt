2a5da8301f4aabb7f271dfb045299ac594f895ba
NY <nakauchi.yuuki@idh-net.co.jp>
2022-10-01 17:35:25 +0900

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,20 @@ export default defineNuxtConfig({
   },
   buildModules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/css/reset.css", "@/assets/css/tailwind.css"],
+  bridge: {
+    meta: true, // Nuxt 3 互換の useMeta を使用可能にする。
+    // -- オプトインの特徴 --
+    // vite: true,         // Webpack 4 の代わりに Vite をバンドラーとして使用する
+    // -- デフォルトの特徴 --
+    // nitro: false,       // Nitro の代わりにレガシーサーバーを使用する
+    // app: false,         // Nuxt 3 互換の `nuxtApp` インターフェースを無効にする
+    // capi: false,        // Composition API サポートを無効にする
+    // capi: {
+    //   legacy: false     // ... あるいは、従来の Composition API のサポートを無効にする
+    // },
+    // transpile: false,   // モジュールをトランスパイルしない
+    // scriptSetup: false, // <script setup> サポートを無効にする
+    // autoImports: false, // composables の自動インポートを無効にする
+    // constraints: false  // モジュールの非互換性についての警告を表示しない
+  },
 });

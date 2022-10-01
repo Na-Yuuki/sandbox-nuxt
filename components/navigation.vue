d4cf22cad2440c2ab24198a59cccf3ec6ea4521b
NY <nakauchi.yuuki@idh-net.co.jp>
2022-10-01 14:24:19 +0900

    update

---
+++ components/navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navigation">
+    <ul>
+      <li>インデックス</li>
+      <li>アバウト</li>
+    </ul>    
+  </div>
+</template>
+
+<style>
+.navigation{
+  background-color:lightgrey;
+  padding: 12px;
+}
+</style>
